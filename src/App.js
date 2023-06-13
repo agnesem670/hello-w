fae3f222b1dc1d54d4a00f9654f9f31fbe6e5b64
agnesem670 <agnesem670@gmail.com>
2023-06-13 10:55:12 +0200

    Props and state

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
+import Message from './components/Message';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-      {/*<Greet />*/}
-      {/*<Welcome />*/}
-      <Hello />
+      <Counter/>
+
+      {/* <Message/> */}
+      {/* <Greet name='Dan' heroName='Batman'>
+        <p>This is children paragraph.</p>
+      </Greet>
+      <Greet name='Mike'heroName='Superman'>
+        <button>Action</button>
+      </Greet>
+      <Greet name='John' heroName='Spiderman'/>
+
+      <Welcome name='Dan' heroName='Batman'/>
+      <Welcome name='Mike'heroName='Superman'/>
+      <Welcome name='John' heroName='Spiderman'/> */}
+      {/*<Hello />*/}
     </div>
   );
 }
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -6,7 +6,15 @@ function Greet() {
 }
 */
 
-/*export*/ const Greet = () => <h1>Hello World</h1> // this export force us to use function name when we want to import this component.
-                                                    // to import we have to use {}
-
+const Greet = (props) => { // this export force us to use function name when we want to import this component. to import we have to use {}
+    console.log(props);
+    return ( 
+    <div>   
+        <h1>Hello {props.name} or {props.heroName} </h1>
+        {props.children}
+    </div> 
+    );
+} 
 export default Greet // this export allows us to import this compononen with changed name
+
+/*exportconst Greet = (props) => <h1>Hello World</h1> // this export force us to use function name when we want to import this component. to import we have to use {}*/
